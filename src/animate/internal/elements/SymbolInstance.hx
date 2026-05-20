025e4a15ec08b3716566f0b1a29c045a3b715f69
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-20 13:19:12 -0700

    mobile fixes

--- src/animate/internal/elements/SymbolInstance.hx
+++ src/animate/internal/elements/SymbolInstance.hx
@@ -39,7 +39,7 @@ class SymbolInstance extends AnimateElement<SymbolInstanceJson>
 
 		this.libraryItem = parent.getSymbol(data.SN);
 		this.matrix = data.MX.toMatrix();
-		this.firstFrame = data.FF;
+		this.firstFrame = data.FF ?? 0;
 		this.isColored = false;
 
 		this.loopType = switch (data.LP)
@@ -61,17 +61,18 @@ class SymbolInstance extends AnimateElement<SymbolInstanceJson>
 			switch (color.M)
 			{
 				case "AD" | "Advanced":
-					setColorTransform(color.RM, color.GM, color.BM, color.AM, color.RO, color.GO, color.BO, color.AO);
+					setColorTransform(color.RM ?? 1.0, color.GM ?? 1.0, color.BM ?? 1.0, color.AM ?? 1.0, color.RO ?? 0.0, color.GO ?? 0.0, color.BO ?? 0.0, color.AO ?? 0.0);
 				case "CA" | "Alpha":
-					setColorTransform(1.0, 1.0, 1.0, color.AM, 0.0, 0.0, 0.0, 0.0);
+					setColorTransform(1.0, 1.0, 1.0, color.AM ?? 1.0, 0.0, 0.0, 0.0, 0.0);
 				case "CBRT" | "Brightness":
-					var brightness = color.BRT;
+					var brightness = color.BRT ?? 0.0;
 					var colorMult = 1.0 - Math.abs(brightness);
 					var colorOff = brightness >= 0.0 ? brightness * 255.0 : 0.0;
 					setColorTransform(colorMult, colorMult, colorMult, 1.0, colorOff, colorOff, colorOff, 0.0);
 				case "T" | "Tint":
-					var tint:FlxColor = FlxColor.fromString(color.TC);
-					var tintMult:Float = color.TM;
+					var tintStr:String = color.TC ?? "#FFFFFF";
+					var tint:FlxColor = FlxColor.fromString(tintStr);
+					var tintMult:Float = color.TM ?? 0.0;
 					var mult:Float = 1.0 - tintMult;
 					setColorTransform(mult, mult, mult, 1.0, tint.red * tintMult, tint.green * tintMult, tint.blue * tintMult, 0.0);
 			}
@@ -97,20 +98,24 @@ class SymbolInstance extends AnimateElement<SymbolInstanceJson>
 	 */
 	public function getFrameIndex(index:Int, frameIndex:Int = 0):Int
 	{
-		var frameIndex = firstFrame + (index - frameIndex);
+		var frameTarget = firstFrame + (index - frameIndex);
+		
+		if (libraryItem == null || libraryItem.timeline == null) 
+			return firstFrame;
+
 		var frameCount = libraryItem.timeline.frameCount;
 
 		switch (loopType)
 		{
 			case LoopType.LOOP:
-				frameIndex = FlxMath.wrap(frameIndex, 0, frameCount - 1);
+				frameTarget = FlxMath.wrap(frameTarget, 0, frameCount > 0 ? frameCount - 1 : 0);
 			case LoopType.PLAY_ONCE:
-				frameIndex = FlxMath.minInt(frameIndex, frameCount - 1);
+				frameTarget = FlxMath.minInt(frameTarget, frameCount > 0 ? frameCount - 1 : 0);
 			case LoopType.SINGLE_FRAME:
-				frameIndex = firstFrame;
+				frameTarget = firstFrame;
 		}
 
-		return frameIndex;
+		return frameTarget;
 	}
 
 	/**
@@ -119,15 +124,16 @@ class SymbolInstance extends AnimateElement<SymbolInstanceJson>
 	 */
 	public function isSimpleSymbol():Bool
 	{
+		if (libraryItem == null || libraryItem.timeline == null)
+			return true;
+
 		var timeline = libraryItem.timeline;
 
-		if (timeline.frameCount == 1)
+		if (timeline.frameCount <= 1)
 			return true;
 
 		if (loopType == SINGLE_FRAME)
 			return true;
-
-		// TODO: more indepth check through layers
 
 		return false;
 	}
@@ -136,9 +142,8 @@ class SymbolInstance extends AnimateElement<SymbolInstanceJson>
 
 	override function getBounds(frameIndex:Int, ?rect:FlxRect, ?matrix:FlxMatrix, ?includeFilters:Bool = true, ?useCachedBounds:Bool = false):FlxRect
 	{
-		// TODO: look into this
-		// Patch-on fix for a really weird fucking bug
-		if (libraryItem != null && libraryItem.timeline.parent.existsSymbol(symbolName))
+		// safeguard
+		if (libraryItem != null && libraryItem.timeline != null && libraryItem.timeline.parent != null && libraryItem.timeline.parent.existsSymbol(symbolName))
 			libraryItem = libraryItem.timeline.parent.getSymbol(symbolName);
 
 		// Prepare the bounds matrix
@@ -153,6 +158,9 @@ class SymbolInstance extends AnimateElement<SymbolInstanceJson>
 		{
 			targetMatrix = this.matrix;
 		}
+
+		if (libraryItem == null || libraryItem.timeline == null)
+			return FlxRect.get(0, 0, 0, 0);
 
 		// Get the bounds of the symbol item timeline
 		return libraryItem.timeline.getBounds(getFrameIndex(frameIndex, 0), null, rect, targetMatrix, includeFilters, useCachedBounds);
@@ -191,6 +199,9 @@ class SymbolInstance extends AnimateElement<SymbolInstanceJson>
 	function _drawTimeline(camera:FlxCamera, index:Int, frameIndex:Int, parentMatrix:FlxMatrix, transform:Null<ColorTransform>, blend:Null<BlendMode>,
 			antialiasing:Null<Bool>, shader:Null<FlxShader>)
 	{
+		if (libraryItem == null || libraryItem.timeline == null)
+			return;
+
 		_mat.copyFrom(matrix);
 		_mat.concat(parentMatrix);
 		libraryItem.timeline.currentFrame = getFrameIndex(index, frameIndex);
